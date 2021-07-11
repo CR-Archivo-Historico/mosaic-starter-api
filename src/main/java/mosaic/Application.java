11c2e5b504540e5797893fc153eb51084647855a
ChristianRoig <membriyin@gmail.com>
2021-07-10 21:25:56 -0300

    Creacion de Usuarios

--- src/main/java/mosaic/Application.java
+++ src/main/java/mosaic/Application.java
@@ -3,6 +3,13 @@ package mosaic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Baeldung
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import java.util.stream.Stream;
+import mosaic.modules.baeldung.entities.User;
+import mosaic.modules.baeldung.repositories.UserRepository;
+
 @SpringBootApplication
 public class Application {
 
@@ -10,5 +17,17 @@ public class Application {
 		System.setProperty("server.servlet.context-path", "/starter-api"); // se coloca el mismo path que war generado (para que sea indistinto ejecutarlo local o desde un servidor)
 		SpringApplication.run(Application.class, args);
 	}
+	
+	//Baeldung
+	@Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("Juan", "Marina", "Felipe", "Fernanda", "Lucas").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@sieteideas.com.ar");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
 
 }
