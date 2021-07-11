74e88d0fb541db77c60308742c6e3b4ccf0d1736
ChristianRoig <membriyin@gmail.com>
2021-07-10 21:18:54 -0300

    Copia de codigo Baeldung

--- src/main/java/mosaic/modules/baeldung/repositories/UserRepository.java
+++ src/main/java/mosaic/modules/baeldung/repositories/UserRepository.java
@@ -1,5 +1,10 @@
 package mosaic.modules.baeldung.repositories;
 
-public class UserRepository {
 
-}
+import mosaic.modules.baeldung.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{}
