bd5d464fff5c14b72ee69e350c6380a10907ad9c
Christian Roig <ch.roig@gmail.com>
2021-08-23 23:44:08 -0300

    Agregado de Bean

--- src/main/java/mosaic/Application.java
+++ src/main/java/mosaic/Application.java
@@ -1,14 +1,36 @@
 package mosaic;
 
+/* doc: 
+ * coloque un archivo cert_1234.pdf en /Fuentes/
+ * corra la aplicacion
+ * acceda a http://localhost:8080/file-server/file/cert_1234.pdf
+ * se muestra el archivo en /Fuentes/cert_1234.pdf
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import mosaic.servlet.FileServlet;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		System.setProperty("server.servlet.context-path", "/starter-api"); // se coloca el mismo path que war generado (para que sea indistinto ejecutarlo local o desde un servidor)
+		System.setProperty("server.servlet.context-path", "/file-server"); // se coloca el mismo path que war generado (para que sea indistinto ejecutarlo local o desde un servidor)
 		SpringApplication.run(Application.class, args);
 	}
 
+	/* ref: 
+	 * https://www.baeldung.com/register-servlet
+	 */
+	
+	@Bean
+	public ServletRegistrationBean exampleServletBean() {
+	    ServletRegistrationBean bean = new ServletRegistrationBean(
+	      new FileServlet(), "/file/*");
+	    bean.setLoadOnStartup(1);
+	    return bean;
+	}
 }
